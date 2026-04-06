ee175e12bcb4d1a16182a7b7bfd45bac5f3df60b
vedal789 <meepydexter@gmail.com>
2026-04-06 07:42:36 +0100

    are we tuff right here

--- src/routes/TutelMod-Packager/+page.svelte
+++ src/routes/TutelMod-Packager/+page.svelte
@@ -3,23 +3,23 @@
     import { PUBLIC_STUDIO_URL } from "$env/static/public";
     onMount(() => {
         window.location.href =
-            `${PUBLIC_STUDIO_URL}/PenguinMod-Packager`;
+            `${PUBLIC_STUDIO_URL}/TutelMod-Packager`;
     });
 </script>
 
 <svelte:head>
     <title>Redirecting</title>
-    <meta name="title" content="PenguinMod - Home" />
-    <meta property="og:title" content="PenguinMod - Home" />
-    <meta property="twitter:title" content="PenguinMod - Home">
+    <meta name="title" content="TutelMod - Home" />
+    <meta property="og:title" content="TutelMod - Home" />
+    <meta property="twitter:title" content="TutelMod - Home">
     <meta name="description" content="The area where featured projects and community stuff & info is shown.">
     <meta property="twitter:description" content="The area where featured projects and community stuff & info is shown.">
-    <meta property="og:url" content="https://penguinmod.com/">
-    <meta property="twitter:url" content="https://penguinmod.com/">
+    <meta property="og:url" content="https://tutelmod.com/">
+    <meta property="twitter:url" content="https://tutelmod.com/">
 </svelte:head>
 <div>
     If you are not redirected automatically,
-    <a href="{PUBLIC_STUDIO_URL}/PenguinMod-Packager">click here</a>
+    <a href="{PUBLIC_STUDIO_URL}/TutelMod-Packager">click here</a>
 </div>
 
 <style>
